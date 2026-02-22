28c624329e473cabfdb03cd7831b8c0f28a55118
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-22 23:12:44 +0530

    update

--- swap.c
+++ swap.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+
 int main()
 {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("The number is Even");
+    else
+        printf("The number is Odd");
+
+    return 0;
 }
