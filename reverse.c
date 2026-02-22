8721f874ab92e39e677e1eb543f3037a1064798d
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-22 23:29:30 +0530

    update

--- reverse.c
+++ reverse.c
@@ -6,3 +6,14 @@ int main()
 
     printf("Enter a number: ");
     scanf("%d", &num);
+    while (num != 0)
+    {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reversed number: %d", rev);
+
+    return 0;
+}
