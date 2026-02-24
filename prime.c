99854201ccfbf52b7538a2506c491ac0242bb6b8
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-24 23:32:54 +0530

    up

--- prime.c
+++ prime.c
@@ -14,3 +14,17 @@ int main()
 
     for (i = 2; i <= num / 2; i++)
     {
+        if (num % i == 0)
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("Prime Number");
+    else
+        printf("Not a Prime Number");
+
+    return 0;
+}
