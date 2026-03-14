25914b185080edfd69c2616dbf7e7731356a7961
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-14 23:34:22 +0530

    1

--- Fibonacci.c
+++ Fibonacci.c
@@ -7,17 +7,3 @@ int main()
 
     printf("Enter number of terms: ");
     scanf("%d", &n);
-
-    printf("Fibonacci Series: ");
-
-    for (i = 1; i <= n; i++)
-    {
-        printf("%d ", t1);
-
-        next = t1 + t2;
-        t1 = t2;
-        t2 = next;
-    }
-
-    return 0;
-}
