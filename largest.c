334c2c1b15ad23b3a6ac0d8d6962175a5e8a1cc7
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-18 23:17:15 +0530

    update

--- largest.c
+++ largest.c
@@ -5,4 +5,14 @@ int main()
 {
     int a, b, c;
 
-    printf("Enter three numb
+    printf("Enter three numbers (separated by spaces) and press Enter:\n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a >= b && a >= c)
+        printf("Largest number is %d", a);
+    else if (b >= a && b >= c)
+        printf("Largest number is %d", b);
+    else
+        printf("Largest number is %d", c);
+
+    return 0;
+}
