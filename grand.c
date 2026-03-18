de079c575473d693e3e19e715658e184f14579f2
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-18 22:57:15 +0530

    update

---
+++ grand.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct Student
+{
+    char name[50];
+    int marks;
+};
+
+int main()
+{
+    int n, i;
+    struct Student s[100];
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter name and marks: ");
+        scanf("%s %d", s[i].name, &s[i].marks);
+    }
+
+    printf("\nStudent Grades:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s - ", s[i].name);
+
+        if (s[i].marks >= 90)
+            printf("A\n");
+        else if (s[i].marks >= 75)
+            printf("B\n");
+        else if (s[i].marks >= 50)
+            printf("C\n");
+        else
+            printf("Fail\n");
+    }
+
+    return 0;
+}
