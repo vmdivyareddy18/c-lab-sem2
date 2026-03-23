7f7cabf13aa926c75b423076ce5c3946885fc64d
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-23 23:19:32 +0530

    update

--- simplefunction..c
+++ simplefunction..c
@@ -1,13 +1,1 @@
-/* simplefunction..c - tiny demo showing a function call (fixed duplicates) */
-#include <stdio.h>
-
-void greet()
-{
-    printf("Hello, Welcome to C Programming!\n");
-}
-
-int main()
-{
-    greet(); // Function Call
-    return 0;
-}
+/* s
