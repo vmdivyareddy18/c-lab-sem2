a70da7390b0701b262b2a0848ec012eb3ac8da18
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-04-05 23:30:57 +0530

    4

--- reverse.c
+++ reverse.c
@@ -8,3 +8,12 @@ int main()
     scanf("%d", &num);
     while (num != 0)
     {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reversed number: %d", rev);
+
+    return 0;
+}
