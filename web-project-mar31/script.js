733ea1c21797c0a04cd3fac4b05f43cd4d71e2fd
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-04-23 22:50:54 +0530

    9

--- web-project-mar31/script.js
+++ web-project-mar31/script.js
@@ -1,5 +1,2 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('toggle');
-  btn.addEventListener('click', () => {
-    document.documentElement.classList.toggle('dark');
-    consol
+  const btn = docu
