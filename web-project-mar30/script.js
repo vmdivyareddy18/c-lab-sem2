f52570e10c4530ff933a336fd1084dbf151fb9fe
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-04-25 23:32:55 +0530

    1

--- web-project-mar30/script.js
+++ web-project-mar30/script.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('toggle');
-  btn.addEventListener('click', ()=>{
-    document.documentElement.classList.toggle('dark');
-    console.log('Toggled theme (Mar 30 project).');
-  });
-  console.log('Web Combo Project (Mar 30) loaded.');
-});
+  btn.addEventListener('click', () => {
+    document.documen
